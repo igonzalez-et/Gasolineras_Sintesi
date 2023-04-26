fddacbd142c89b854174e13c234b2caeba7cf379
aleeiix10 <95906986+aleeiix10@users.noreply.github.com>
2023-04-26 19:03:37 +0200

    dsads

--- cron.php
+++ cron.php
@@ -1,16 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Base de datos gasolineras</title>
-</head>
-<body>
-    <?php
-        set_time_limit(300);
+<?php
         include("utilidades.php");
         $conn = conectarBDD();
+        set_time_limit(300);
 
     ?>
     <?php
@@ -23,47 +14,52 @@
             $gasolineraLog = $gasolinera;
             $arrayKeys = array_keys($gasolineraLog);
         }
+        // Recorremos los datos de la API
         foreach ($json['ListaEESSPrecio'] as $gasolinera) {
-            $stringTotalPrecio = "";
-            foreach ($gasolinera as $key => $datos) {
-                $subs = substr($key,0,6);
-                if($subs == "Precio"){
-                    if($datos != ""){
-                        $stringTotalPrecio .= "'" . mysqli_real_escape_string($conn, $datos) . "',";
-                    }
-                    else {
-                        $stringTotalPrecio .= "NULL,";
-                    }
-                }
-            }
-            $stringTotalPrecio = substr($stringTotalPrecio, 0, -1);
-            $stringColumnsPrecio = "";
-            foreach ($arrayKeys as $key) {
-                $key = str_replace(' ', '_', $key);
-                $key = str_replace('%', '', $key);
-                $key = str_replace('.', '', $key);
-                $key = str_replace('(', '_', $key);
-                $key = str_replace(')', '', $key);
-                $subs = substr($key,0,6);
-                if ($subs === "Precio") { 
-                    $stringColumnsPrecio .= $key . ",";
-                }
-            }
-            $stringColumnsPrecio = substr($stringColumnsPrecio, 0, -1);
+            
+
             // Consulta a la base de datos
             $sqlQuery = "SELECT * FROM gasolineras";
             $result = mysqli_query($conn, $sqlQuery);
-            if (mysqli_num_rows($result) > 0) {
-                while ($row = mysqli_fetch_assoc($result)) {
-                    $sqlQuery2 = "INSERT INTO precios_gasolinera(".$stringColumnsPrecio.",gasolinera_id,ultima_actualizacion) VALUES(".$stringTotalPrecio.",".$row['id'].",current_timestamp());";
 
-                }
-                if (mysqli_query($conn, $sqlQuery2)) {
-                    echo $sqlQuery2;
-                }
-                
+            // Itera sobre los resultados de la consulta y agrega los datos a la lista HTML
+            if (mysqli_num_rows($result) > 0) {
+                while($row = mysqli_fetch_assoc($result)) {
+                    $stringTotalPrecio = "";
+                    foreach ($gasolinera as $key => $datos) {
+                        $subs = substr($key,0,6);
+                        if($subs == "Precio"){
+                            if($datos != ""){
+                                $stringTotalPrecio .= "'" . mysqli_real_escape_string($conn, $datos) . "',";
+                            }
+                            else {
+                                $stringTotalPrecio .= "NULL,";
+                            }
+                        }
+                    }
+                    $stringTotalPrecio = substr($stringTotalPrecio, 0, -1);
+                    $stringColumnsPrecio = "";
+                    foreach ($arrayKeys as $key) {
+                        $key = str_replace(' ', '_', $key);
+                        $key = str_replace('%', '', $key);
+                        $key = str_replace('.', '', $key);
+                        $key = str_replace('(', '_', $key);
+                        $key = str_replace(')', '', $key);
+                        $subs = substr($key,0,6);
+                        if ($subs === "Precio") { 
+                            $stringColumnsPrecio .= $key . ",";
+                        }
+                    }
+                    $stringColumnsPrecio = substr($stringColumnsPrecio, 0, -1);
+                    $sqlQuery2 = "INSERT INTO precios_gasolinera(".$stringColumnsPrecio.",gasolinera_id,ultima_actualizacion) VALUES(".$stringTotalPrecio.",".$row['id'].",current_timestamp());";
+                    if (mysqli_query($conn, $sqlQuery2)) {
+                        echo $sqlQuery2;
+                    } else {
+                        echo "Error: " . $sqlQuery2 . "<br>" . mysqli_error($conn);
+                    }
+                }        
             }
         }
-            // Cierra la conexión
-            mysqli_close($conn);
-        ?>
+        // Cierra la conexión
+        mysqli_close($conn);
+    ?>
--- listado_gasolinera.php
+++ listado_gasolinera.php
@@ -25,9 +25,27 @@
     ?>
     <h1>Lista de gasolineras</h1>
     <div class="gasolineras">
-        <ul class="listagasolineras">
+        <div class="filtros">
+            <form method="POST">
             <?php
                 $conn = conectarBDD();
+                $sql = "select Rótulo from gasolineras group by Rótulo;";
+                $result = mysqli_query($conn, $sql);
+                echo "<select name=\"Rótulo\">\n";
+                if (mysqli_num_rows($result) > 0) {
+                    while($row = mysqli_fetch_assoc($result)) {
+                        echo "<option value=\"$row[Rótulo]\">$row[Rótulo]</option>\n";
+                    }
+                }
+                echo "</select>";
+
+            ?>
+                <input type="submit" value="Buscar">
+            </form>
+        </div>
+        <ul class="listagasolineras">
+            <?php
+                
 
                 if(isset($_SESSION["correo"])) {
                     $sql = "SELECT * FROM usuarios where correo = '".$_SESSION["correo"]."';";
@@ -43,10 +61,10 @@
                 }
 
                 
-
-                // Consulta a la base de datos
-                $sql = "SELECT * FROM gasolineras";
-                $result = mysqli_query($conn, $sql);
+                if (isset($_POST["Rótulo"])){
+                    $sql = "SELECT * FROM gasolineras WHERE Rótulo = '".$_POST["Rótulo"]."';";
+                    $result = mysqli_query($conn, $sql);
+                    // Consulta a la base de datos
 
                 // Itera sobre los resultados de la consulta y agrega los datos a la lista HTML
                 if (mysqli_num_rows($result) > 0) {
@@ -118,6 +136,84 @@
 
                 // Cierra la conexión a la base de datos
                 mysqli_close($conn);
+                }
+                else{
+                    // Consulta a la base de datos
+                    $sql = "SELECT * FROM gasolineras";
+                    $result = mysqli_query($conn, $sql);
+
+                    // Itera sobre los resultados de la consulta y agrega los datos a la lista HTML
+                    if (mysqli_num_rows($result) > 0) {
+                        
+                        while($row = mysqli_fetch_assoc($result)) {
+                            $direccion = $row['Dirección'];
+                            $latitud = $row['Latitud'];
+                            $latitud = str_replace(',', '.', $latitud);
+                            $localidad = $row['Localidad'];
+                            $longitud = $row['Longitud__WGS84'];
+                            $longitud = str_replace(',', '.', $longitud);
+                            $provincia = $row['Provincia'];
+
+                            $sql2 = "SELECT count(*) as contador FROM favoritos_gasolinera where usuario_id = ".$usuario_id." and gasolinera_id = ".$row["id"].";";
+                            $strFavorito = "";
+                            $result2 = mysqli_query($conn, $sql2);
+                            $row2 = mysqli_fetch_assoc($result2);
+                            if($row2["contador"] == 0) {
+                                $strFavorito = '<input type="submit" name="marcar_favorito" id="botonMarcar'.$row["id"].'" class="botonMarcar" value="Marcar Favorito">';
+                            }else {
+                                $strFavorito = '<input type="submit" name="marcar_favorito" id="botonMarcar'.$row["id"].'" class="botonMarcar favorito" value="Marcar Favorito">';
+                            }
+                            echo 
+                            "<li>CP: " . $row["CP"] . " - Dirección: " . $row["Dirección"] . " - Provincia: " . $row["Provincia"] . " - Rótulo: " . $row["Rótulo"] . " - Municipio: " . $row["Municipio"] .
+                                '<form id="form_' . $row["id"] . '" action="detalle_gasolinera.php" method="post">
+                                    <input type="hidden" name="id" value="' . $row["id"] . '">
+                                    <button type="submit">Ver detalles</button>
+                                </form>
+                                <form id="form_favorito_' . $row["id"] . '" method="post" onsubmit="return submitFormFavorito(' . $row["id"] . ');">
+                                    <input type="hidden" name="id_gasolinera" value="' . $row["id"] . '">' . $strFavorito . '
+                                </form>' .
+                                '<a href="https://www.google.com/maps?q='.urlencode($direccion . ', ' . $localidad . ', ' . $provincia).'&ll='.$latitud . ',' . $longitud.'&z=17" target="_blank">Ver ubicación</a>'.
+                            "</li>";
+                        }
+                    } else {
+                        echo "No se encontraron resultados.";
+                    }
+
+
+                    if (isset($_POST['funcion'])) {
+                        $funcion = $_POST['funcion'];
+                        
+                        if ($funcion == 'marcarFavorito') {
+                            $id_gasolinera = $_POST['id_gasolinera'];
+
+                            $sql2 = "SELECT count(*) as contador FROM favoritos_gasolinera where usuario_id = ".$usuario_id." and gasolinera_id = ".$id_gasolinera.";";
+                            $result2 = mysqli_query($conn, $sql2);
+                            $row2 = mysqli_fetch_assoc($result2);
+                            if($row2["contador"] == 0) {
+                                $sql = "INSERT INTO favoritos_gasolinera(usuario_id,gasolinera_id) VALUES(".$usuario_id.",".$id_gasolinera.")";
+                                if (mysqli_query($conn, $sql)) {
+                                    echo "<script>console.log('La gasolinera se ha marcado como favorita correctamente.');</script>";
+                                } else {
+                                    echo "<script>console.log('Error al marcar la gasolinera como favorita: " . mysqli_error($conn). "');</script>";
+                                }
+                            }else {
+                                $sql = "delete from favoritos_gasolinera where usuario_id = ".$usuario_id." and gasolinera_id = ".$id_gasolinera.";";
+                                if (mysqli_query($conn, $sql)) {
+                                    echo "<script>console.log('La gasolinera se ha eliminado como favorita correctamente.');</script>";
+                                } else {
+                                    echo "<script>console.log('Error al eliminar la gasolinera como favorita: " . mysqli_error($conn). "');</script>";
+                                }
+                            }
+
+                        }else {
+                        echo 'Función no encontrada';
+                        }
+                    }
+
+                    // Cierra la conexión a la base de datos
+                    mysqli_close($conn);
+                                    }
+                                    
             ?>
         </ul>
     </div>
